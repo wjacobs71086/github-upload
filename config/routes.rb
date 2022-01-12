c17eb93563186f38d247f1e56cb1cdc830ebe8b2
Wesley Jacobs <wesley.j@simplepractice.com>
2022-01-12 14:20:14 -0800

    Completed section 8. Next, Refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "articles#index"
-  resources :articles
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  # This is a nesting of the resources. Indicates that Comments are associated and held with articles.
+  resources :articles do  
+    resources :comments
+  end
 end
